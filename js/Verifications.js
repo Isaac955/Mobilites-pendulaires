12d0bdd5304ba19e08fdac1a935f25b5e41d9490
Isaac S <123961485+Isaac955@users.noreply.github.com>
2023-12-24 18:23:45 +0100

    ajout du js

---
+++ js/Verifications.js
@@ -0,0 +1,18 @@
+﻿function VerifMax(nom) 
+{
+	var max = 3;
+	var compteur = 0;
+	var checkboxes = document.getElementsByName(nom);
+	for (var i = 0; i < checkboxes.length; i++) 
+	{
+		if (checkboxes.item(i).checked == true) 
+		{
+			compteur++
+			if (compteur > max) 
+			{
+				checkboxes.item(i).checked = false;
+				alert("3 réponses possibles maximum");
+			}
+		}
+	}
+}
